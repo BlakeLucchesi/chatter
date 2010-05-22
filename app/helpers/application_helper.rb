c5cffa3bc6b7bc016c1620fd8d84cc05d2f25954
Blake Lucchesi <blake@boldsource.com>
2010-05-22 16:40:19 -0700

    Fix markup/styles.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  def body_id
+    "#{controller_name}-#{action_name}"
+  end
+  
+  def body_classes
+    classes = []
+    classes << controller.controller_name
+    classes.join(' ')
+  end
+  
 end
